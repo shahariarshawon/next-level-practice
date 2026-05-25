80d28ab4ad3b5cf929f817d4de8c64e1701912b5
shahariarshawon <shahariararafat7739@gmail.com>
2026-05-25 11:29:54 +0600

    Premitive datatype

---
+++ module1/premitveTypes.ts
@@ -0,0 +1,9 @@
+//  premitive datatypes
+let userName: string = "shawon";
+userName = "shawonnnn";
+
+console.log(userName);
+
+let rollNumber: number = 1224.999999999999;
+
+console.log(rollNumber.toFixed(4));
